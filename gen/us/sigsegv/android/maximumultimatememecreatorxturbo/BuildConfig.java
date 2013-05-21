9eff109a35cd4df8575ea6f595e173b1aa0cde49
0xbadbeef <0xbadbeef@sigsegv.us>
2013-05-20 22:03:35 -0700

    Initial Commit!

---
+++ gen/us/sigsegv/android/maximumultimatememecreatorxturbo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package us.sigsegv.android.maximumultimatememecreatorxturbo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
